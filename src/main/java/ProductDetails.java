b9803a3c521b5298c04c94f49f355ec416932d58
Abhinav <abhinav.kumar3@gmail.com>
2021-11-10 02:24:12 +0530

    Product Info Changes

--- src/main/java/ProductDetails.java
+++ src/main/java/ProductDetails.java
@@ -1,7 +1,11 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 import javax.servlet.ServletException;
@@ -13,16 +17,16 @@ import javax.servlet.http.HttpServletResponse;
 import com.ecommerce.DBConnection;
 
 /**
-* Servlet implementation class DemoJDBC
+* Servlet implementation class ProductDetails
 */
-@WebServlet("/DemoJDBC")
-public class DemoJDBC extends HttpServlet {
+@WebServlet("/ProductDetails")
+public class ProductDetails extends HttpServlet {
         private static final long serialVersionUID = 1L;
        
     /**
 * @see HttpServlet#HttpServlet()
 */
-    public DemoJDBC() {
+    public ProductDetails() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -42,10 +46,16 @@ public class DemoJDBC extends HttpServlet {
                         props.load(in);
                         
                         DBConnection conn = new DBConnection(props.getProperty("url"), props.getProperty("userid"), props.getProperty("password"));
-                        out.println("DB Connection initialized.<br>");
+                        Statement stmt = conn.getConnection().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                        stmt.executeUpdate("insert into eproduct (name, price, date_added) values ('New Product', 17800.00, now())");
+                        ResultSet rst = stmt.executeQuery("select * from eproduct");
                         
-                        conn.closeConnection();
-                        out.println("DB Connection closed.<br>");
+                        while (rst.next()) {
+                                out.println(rst.getInt("ID") + ", " + rst.getString("name") + "<Br>");
+                        }
+                        
+                        stmt.close();
+                        
                         
                         
                         out.println("</body></html>");
